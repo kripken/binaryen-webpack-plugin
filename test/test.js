4aa15b6b579638913864a1068bb83d315059db33
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-04-18 19:46:05 -0700

    moar

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 
-// do nothing
+// load some binary code.
+// trying to load a .wasm file hits a webpack error, support there is
+// not stable yet, so avoid that for now
 
 var wasm = require('binary-loader!./test.binary');
 
