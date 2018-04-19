fc8e226214b94304474ddee5296de6c1c3a98a31
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-04-19 10:14:36 -0700

    it optimizes

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var Binaryen = require('binaryen');
 
-function optimize(filename) {
-/*
-  var originalBuffer = ..get the original buffer here.. // XXX or should we receive it?
-  var module = Binaryen.readBinary(originalBuffer, size); // XXX why is there a size?
+function optimize(originalBuffer) {
+  var module = Binaryen.readBinary(originalBuffer);
   module.optimize();
   var optimizedBuffer = module.emitBinary();
-  ..write the optimized buffer here..  // XXX or should we return it?
-*/
+  return Buffer.from(optimizedBuffer);
 }
 
 function BinaryenPlugin() {
@@ -18,22 +15,15 @@ BinaryenPlugin.prototype.apply = function(compiler) {
     for (var filename in compilation.assets) {
       if (/\.wasm$/.test(filename)) {
         // process a wasm asset
-        console.log('asset: ' + filename + ' : ');
         var asset = compilation.assets[filename];
-console.log(asset);
-for (var k in asset) console.log('key: ' + k);
-console.log(asset.source());
+        // TODO: is there a proper API for these property accesses?
         var value = asset._source._value;
-console.log('VALUE ');
-console.log(value);
-        console.log(JSON.stringify(value));
-        console.log(JSON.stringify(value.type));
         if (!Buffer.isBuffer(value)) {
           console.warn('binaryen-webpack-plugin: .wasm asset that is not a Buffer (' + filename + ')');
           continue;
         }
-        console.log('optimize it!');
-        // TODO: optimize(filename);
+        // TODO: is there a proper API for this assignment?
+        asset._source._value = optimize(value);
       }
     }
     callback();
