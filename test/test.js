9a07fedefa3f4bbc632354b12df32faf7aedf6d7
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-04-19 08:36:30 -0700

    webpack works on the test

--- test/test.js
+++ test/test.js
@@ -3,5 +3,5 @@
 // trying to load a .wasm file hits a webpack error, support there is
 // not stable yet, so avoid that for now
 
-var wasm = require('binary-loader!./test.binary');
+import('./test.wasm');
 
