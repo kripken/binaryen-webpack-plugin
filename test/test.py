e8680516f949f5881bf7b19e14cdbebc769d91ce
Alon Zakai <alonzakai@gmail.com>
2018-04-19 17:04:40 -0700

    look for node in more ways

--- test/test.py
+++ test/test.py
@@ -3,6 +3,7 @@ import json
 import logging
 import shutil
 import subprocess
+from distutils.spawn import find_executable
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,7 +34,8 @@ try:
   logging.warning('install dev packages')
   subprocess.check_call(['npm', 'install', '-D', 'webpack', 'webpack-cli'])
   logging.warning('build')
-  subprocess.check_call(['./node_modules/.bin/webpack', '--config=webpack.config.js'])
+  node = find_executable('nodejs') or find_executable('node') or 'node'
+  subprocess.check_call([node, './node_modules/.bin/webpack', '--config=webpack.config.js'])
   logging.warning('verify the output')
   orig_size = os.stat('test.wasm').st_size
   opt_size = None
@@ -42,7 +44,7 @@ try:
       opt_size = os.stat(os.path.join('dist', name)).st_size
   assert opt_size, 'must fine the wasm, and size must be > 0'
   assert opt_size < orig_size - 15, 'must be a bunch of bytes smaller'
-  assert float(opt_size) / orig_size < 0.75, 'must be proportionally noiticeable smaller'
+  assert float(opt_size) / orig_size < 0.75, 'must be proportionally noticeably smaller'
   logging.warning('ok!')
 finally:
   os.chdir(old)
